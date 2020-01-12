a2df6d1f29d9e53d1951abafcaf09d302cb5db2f
Victor <olaitanvictor99@gmail.com>
2020-01-12 09:21:43 -0500

    Revert "accept changes w/ timebar v angular"
    This reverts commit d48a03b21c33bbaaa7736235630c2046a51c4526.

--- api/query.js
+++ api/query.js
@@ -4,19 +4,6 @@ const Event = require("../model/event");
 const Device = require("../model/device");
 
 router.post("/events", (req, res, next) => {
-  console.log(req.body.query);
-  handleQuery(Event, req, res, next);
-});
-
-router.post("/people", (req, res, next) => {
-  handleQuery(Person, req, res, next);
-});
-
-router.post("/roomSnapshots", (req, res, next) => {
-  handleQuery(RoomSnapshot, req, res, next);
-});
-
-function handleQuery(col, req, res, next) {
   req.body.fn = req.body.fn || "find";
   req.body.query = req.body.query || {};
   req.body.projection = req.body.projection || {};
@@ -33,6 +20,6 @@ function handleQuery(col, req, res, next) {
   if (req.body.sort) {
     q.sort(req.body.sort);
   }
-}
+});
 
 module.exports = router;
--- public/js/controller.js
+++ public/js/controller.js
@@ -2,21 +2,8 @@
 app.controller("mainCtrl", function($scope, $rootScope, $http) {
   let context = ($scope.context = {
     timestamp: null,
-    room: null,
-    person: null,
-    switch: (timestamp, room, person) => {
-      console.log("got context switch request");
-      context.timestamp = timestamp || context.timestamp;
-      context.room = room.toString() || context.room;
-      context.person = person || context.person;
-      Promise.all([people.refresh(), activity.refresh(), timebar.refresh()])
-        .then(() => {
-          console.log("Context switched", context.timestamp, activity.log);
-          search.visible = false;
-          $scope.$apply();
-        })
-        .catch(err => console.error(err));
-    },
+    room: "",
+    person: "",
     refresh: () => {
       return new Promise((resolve, reject) => {});
     }
@@ -24,63 +11,7 @@ app.controller("mainCtrl", function($scope, $rootScope, $http) {
 
   let search = ($scope.search = {
     query: "",
-    visible: false,
-    changeTask: null,
-    error: false,
-    result: [],
-    close: () => (search.visible = false),
-    onChange: () => {
-      $interval.cancel(search.changeTask);
-      if (search.query != "") {
-        search.changeTask = $interval(search.search, 500, 1);
-      }
-    },
-    search: () => {
-      if (search.query == "") return;
-      search.error = false;
-      search.result = [];
-      console.log("Searching...");
-      let regexes = [];
-      search.query
-        .trim()
-        .split(" ")
-        .forEach(q =>
-          regexes.push(new RegExp(`[a-z]*${regexEscape(q)}[a-z]*`, "i"))
-        );
-      doQuery(
-        "events",
-        "find",
-        {
-          $or: [
-            { deviceId: { $in: regexes } },
-            { eventId: { $in: regexes } },
-            { guestId: { $in: regexes } }
-          ]
-        },
-        null,
-        null,
-        { timestamp: 1 },
-        50
-      )
-        .then(events => {
-          search.result = events.map(ev => {
-            let date = new Date(ev.timestamp);
-            let time = dateToTime(date);
-            return {
-              ...ev,
-              formattedTimestamp: `${time.simpleHours}:${time.minutes}:${
-                time.seconds
-              } ${time.isAm ? "AM" : "PM"} ${DAYS_OF_WEEK[date.getDay()]}`
-            };
-          });
-          search.visible = true;
-          $scope.$apply();
-        })
-        .catch(err => {
-          search.error = true;
-          console.error(err);
-        });
-    }
+    search: () => {}
   });
 
   let people = ($scope.people = {
@@ -90,22 +21,14 @@ app.controller("mainCtrl", function($scope, $rootScope, $http) {
     unknownNames: "",
     refresh: () => {
       return new Promise((resolve, reject) => {
-        doAggregate(
-          "events",
-          [
-            {
-              $match: {
-                deviceId: context.room,
-                eventId: {
-                  $in: ["successful keycard unlock", "unlocked no keycard"]
-                },
-                timestamp: { $lte: context.timestamp }
-              }
-            },
-            {}
-          ],
-          (err, events) => {}
-        );
+        doAggregate("events", [
+          {$match: {
+            deviceId: context.room,
+            eventId: {$in: ["successful keycard unlock", "unlocked no keycard"]},
+            timestamp: {$lte: context.timestamp}
+          }},
+          {}
+        ], (err, events) => {})
         doQuery("people", "find", {
           deviceId: context.room
         });
@@ -158,5 +81,4 @@ app.controller("mainCtrl", function($scope, $rootScope, $http) {
       .then(({ data }) => callback(false, data))
       .catch(err => callback(err));
   }
-  const DAYS_OF_WEEK = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 });
